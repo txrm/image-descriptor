ce6c15b4e72e22ee54f40a1d0d0823ab34165f96
Rafael Manzano <ragy@gmx.com>
2023-12-14 08:27:40 +0100

    Removed unsupported fields

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,8 +6,6 @@ provider "google" {
 
 provider "kubernetes" {
   host = "${google_container_cluster.gateway-cluster.endpoint}"
-  username = "${google_container_cluster.gateway-cluster.master_auth.0.username}"
-  password = "${google_container_cluster.gateway-cluster.master_auth.0.password}"
   client_certificate = "${base64decode(google_container_cluster.gateway-cluster.master_auth.0.client_certificate)}"
   client_key = "${base64decode(google_container_cluster.gateway-cluster.master_auth.0.client_key)}"
   cluster_ca_certificate = "${base64decode(google_container_cluster.gateway-cluster.master_auth.0.cluster_ca_certificate)}"
