8bf8280150cd424b0410c5b3200325889bc0f311
Rafael Manzano <ragy@gmx.com>
2023-12-14 07:38:26 +0100

    secret manager + k8s cluster on terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,13 @@ provider "google" {
   region      = "europe-west1"
   zone        = "europe-west1-b"
 }
+
+provider "kubernetes" {
+  host = "${google_container_cluster.gateway-cluster.endpoint}"
+  username = "${google_container_cluster.gateway-cluster.master_auth.0.username}"
+  password = "${google_container_cluster.gateway-cluster.master_auth.0.password}"
+  client_certificate = "${base64decode(google_container_cluster.gateway-cluster.master_auth.0.client_certificate)}"
+  client_key = "${base64decode(google_container_cluster.gateway-cluster.master_auth.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(google_container_cluster.gateway-cluster.master_auth.0.cluster_ca_certificate)}"
+}
+
---
+++ terraform/secret_manager.tf
@@ -0,0 +1,6 @@
+resource "google_secret_manager_secret" "gcp-secret" {
+  secret_id = "SSL-secret"
+
+  labels = {
+    label = "SSL certificate"
+  }
