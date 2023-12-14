f3e08aa4775046badfaf2b7471b57d6f872b959c
Rafael Manzano <ragy@gmx.com>
2023-12-14 07:48:36 +0100

    added Container node pool for gateway cluster

---
+++ terraform/k8s.tf
@@ -0,0 +1,15 @@
+resource "google_container_cluster" "gateway-cluster" {
+  name               = "gateway-cluster"
+  zone               = "europe-west1-b"
+  initial_node_count = 1
+}
+
+resource "google_container_node_pool" "pool" {
+  name = "pool"
+  cluster = "${google_container_cluster.gateway-cluster.name}"
+  zone = "europe-west1-b"
+  node_count = "1"
+  node_config {
+    machine_type = "g1-small"
+  }
+}
