d7b6219776ace4a39a2357166bf018a3cc143c14
Rafael Manzano <ragy@gmx.com>
2023-12-14 08:42:20 +0100

    Added and partitioned fixed Cluster manifest

--- terraform/k8s_manifest.tf
+++ terraform/k8s_manifest.tf
@@ -1,3 +1,11 @@
-resource "kubernetes_manifest" "manifest" {
-  manifest = yamldecode(file("${path.module}/front.yml"))
+resource "kubernetes_manifest" "deployment_manifest" {
+  manifest = yamldecode(file("${path.module}/manifest/deployment.yml"))
+}
+
+resource "kubernetes_manifest" "service_manifest" {
+  manifest = yamldecode(file("${path.module}/manifest/service.yml"))
+}
+
+resource "kubernetes_manifest" "ingress_manifest" {
+  manifest = yamldecode(file("${path.module}/manifest/ingress.yml"))
 }
